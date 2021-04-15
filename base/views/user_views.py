18eaaca96701a65308de213d6c62a3db3ea4fa20
ashur-k <ashurkanwal@yahoo.com>
2021-04-15 16:30:39 +0100

    string value- SECRET_KEY error resolved

--- base/views/user_views.py
+++ base/views/user_views.py
@@ -1,13 +1,11 @@
 from django.shortcuts import render
-
 from rest_framework.decorators import api_view, permission_classes
-from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.response import Response
+
 from django.contrib.auth.models import User
-
-
 from base.serializers import ProductSerializer, UserSerializer, UserSerializerWithToken
-
+# Create your views here.
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
 
@@ -18,17 +16,16 @@ from rest_framework import status
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
     def validate(self, attrs):
         data = super().validate(attrs)
-        
+
         serializer = UserSerializerWithToken(self.user).data
-        
-        for key, value in serializer.items():
-            data[key] = value
+        for k, v in serializer.items():
+            data[k] = v
+
         return data
-    
+
 
 class MyTokenObtainPairView(TokenObtainPairView):
     serializer_class = MyTokenObtainPairSerializer
-
 
 @api_view(['POST'])
 def registerUser(request):
