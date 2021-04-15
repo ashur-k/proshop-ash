b605096688ebbc914d4a72b6586f7ca89ad52ecd
ashur-k <ashurkanwal@yahoo.com>
2021-04-15 16:37:55 +0100

    string value- SECRET_KEY error resolved

--- backend/settings.py
+++ backend/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('PROSHOP_SECRET_KEY'),
+SECRET_KEY = '_*acw00)z9s(be14(0^)$0iau)+=w@&@b^#w%p5@%jvy%tb@4^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
